1e91244a49e0a49cd59bba5dde6952bb581ac9ae
Minh VU <ngoc.vu@polytechnique.org>
2018-08-14 23:34:14 +0100

    add code for export trained graph using tensorflow 1.3

---
+++ export_model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+python object_detection/export_inference_graph.py \
+--pipeline_config_path=config/sim/faster_rcnn_resnet101_coco_carnd_sim.config \
+--trained_checkpoint_prefix=finetuned_sim/faster_rcnn_resnet101_coco/model.ckpt-10000 \
+--output_directory=frozen_sim/faster_rcnn_resnet101_coco
+
+python object_detection/export_inference_graph.py \
+--pipeline_config_path=config/sim/ssd_inception_v2_coco_carnd_sim.config \
+--trained_checkpoint_prefix=finetuned_sim/ssd_inception_v2_coco/model.ckpt-6000 \
+--output_directory=frozen_sim/ssd_inception_v2_coco
+
+python object_detection/export_inference_graph.py \
+--pipeline_config_path=config/sim/ssd_mobilenet_v1_coco_carnd_sim.config \
+--trained_checkpoint_prefix=finetuned_sim/ssd_mobilenet_v1_coco/model.ckpt-6000 \
+--output_directory=frozen_sim/ssd_mobilenet_v1_coco
---
+++ light_classification/tl_classifier.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import os
+import numpy as np
+
+class TLClassifier(object):
+    def __init__(self,
+                 model_path,
+                 img_height,
+                 img_width):
+        self.model_path = model_path
+        self.img_height = img_height
+        self.img_width = img_width
+
+        # load frozen graph
+        self.graph = None
+        self.session = None
+        self.image_tensor = None
+        self.detection_boxes = None
+        self.detection_scores = None
+        self.detection_classes = None
+        self.load_model(model_path=self.model_path)
+
+        # threshold for detection
+        self.threshold_prob = 0.7
+
+    def load_model(self, model_path):
+        # load serialized Tensor Graph
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(graph_def, name='')
+
+        self.session = tf.Session(graph=self.graph)
+
+        # update tensor
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_classes = self.graph.get_tensor_by_name('detection_classes:0')
+        self.detection_scores = self.graph.get_tensor_by_name('detection_scores:0')
+
+        # run first prediction with dummy image to initialize (since first run is considerably slow)
+        self.warmup_model()
+
+    def warmup_model(self):
+        # create a null image
+        null_img = np.zeros((self.img_height, self.img_width, 3), dtype=np.uint8)
+
+        # warm up tensorflow session by running with a null-image
+        _ = self.session.run(
+            [self.detection_boxes, self.detection_classes, self.detection_scores],
+            feed_dict={
+                self.image_tensor: np.expand_dims(null_img, axis=0)
+            }
+        )
+
+    def get_classification(self, image_rgb):
+        """Determines the color of the traffic light in the image
+
+        Args:
+            image (cv::Mat): image containing the traffic light
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+        # TODO implement light color prediction
+        # run the detection
+        (boxes, classes, scores) = self.session.run(
+            [self.detection_boxes, self.detection_classes, self.detection_scores],
+            feed_dict = {
+                self.image_tensor : np.expand_dims(image_rgb, axis=0)
+            }
+        )
+
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
+
+        # we get the one give maximum score
+        i_max_score = np.argmax(scores)
+        if (scores[i_max_score] > self.threshold_prob):
+            return classes[i_max_score]
+
+        return 4
