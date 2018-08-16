9712614bc43b9c31eba817be95c0e378e59b692a
Minh VU <ngoc.vu@polytechnique.org>
2018-08-17 00:21:35 +0100

    wip

--- export_model.sh
+++ export_model.sh
@@ -3,12 +3,12 @@ set -x
 
 python object_detection/export_inference_graph.py \
 --pipeline_config_path=config/sim/ssd_mobilenet_v1_coco_carnd_sim.config \
---trained_checkpoint_prefix=finetuned_sim/ssd_mobilenet_v1_coco/model.ckpt-6000 \
+--trained_checkpoint_prefix=finetuned_sim/ssd_mobilenet_v1_coco/model.ckpt-10000 \
 --output_directory=frozen_sim/ssd_mobilenet_v1_coco
 
 python object_detection/export_inference_graph.py \
 --pipeline_config_path=config/sim/ssd_inception_v2_coco_carnd_sim.config \
---trained_checkpoint_prefix=finetuned_sim/ssd_inception_v2_coco/model.ckpt-6000 \
+--trained_checkpoint_prefix=finetuned_sim/ssd_inception_v2_coco/model.ckpt-10000 \
 --output_directory=frozen_sim/ssd_inception_v2_coco
 
 python object_detection/export_inference_graph.py \
